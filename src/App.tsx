07e67eab00348a5e02a2263015b17a9450c114bc
Przemyslaw Rachwal <przemyslaw.rachwal@gmail.com>
2025-11-01 13:46:44 +0100

    feat: add TypeDoc and TSDoc configuration files; enhance documentation generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { ThemeProvider } from './providers/ThemeProvider';
 import AppShell from './components/layout/AppShell';
 
+/**
+ * Główny komponent aplikacji Preact Components System.
+ *
+ * @returns Główny komponent aplikacji opakowany w ThemeProvider
+ */
 const App = () => (
   <ThemeProvider>
     <AppShell />
--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,7 @@ import { render } from 'preact'
 import './index.scss'
 import App from './App'
 
+/**
+ * Punkt wejścia aplikacji - renderuje główny komponent App do elementu DOM o id 'app'.
+ */
 render(<App />, document.getElementById('app')!)
